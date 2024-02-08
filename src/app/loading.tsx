e1bb0d97469956bc441cf487906289bb6d61d089
Resheph Inayat <rasafinayat@gmail.com>
2024-02-08 22:03:34 +0500

    Initial Commit

---
+++ src/app/loading.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function loading() {
+  return (
+    <div className='flex justify-center items-center'>
+      <div className='text-6xl pt-80 font-bold text-white'>
+ Loading.....
+      </div>
+    </div>
+  )
+}
